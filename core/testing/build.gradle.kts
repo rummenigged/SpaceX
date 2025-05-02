23909e386bdfa961c620f1dd3fc667339c40c303
Rummenigge Duarte <rummenigge.duarte@gmail.com>
2025-05-02 11:35:40 -0300

    refactor: Add new core:test module

---
+++ core/testing/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.octopus.edu.kotlin.core.testing"
+    compileSdk = rootProject.ext["compileSdkVersion"].toString().toInt()
+
+    defaultConfig {
+        minSdk = rootProject.ext["minSdkVersion"].toString().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = rootProject.ext["sourceCompatibility"] as JavaVersion
+        targetCompatibility = rootProject.ext["targetCompatibility"] as JavaVersion
+    }
+
+    kotlinOptions {
+        jvmTarget = rootProject.ext["kotlinOptionsJVMTarget"].toString()
+    }
+}
+
+dependencies {
+
+    api(libs.test.konsist)
+
+    api(libs.test.junit)
+    api(libs.test.androidx.junit)
+    api(libs.test.androidx.espresso.core)
+    implementation(platform(libs.androidx.compose.bom))
+    api(libs.test.androidx.compose.ui.junit4)
+    debugApi(libs.test.androidx.compose.ui.manifest)
+}
